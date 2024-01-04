3ed90d47c8bd780d028ae9d710942b28956e803b
Balovsky <mariuszfranik98@gmail.com>
2024-01-04 17:55:52 +0100

    create hamburger menu section for sub pages

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 console.log('connected to html');
 const btn = document.querySelector('.hamburgerMenu')
+const mobileMenu = document.querySelector('.mobileSubPages')
+const sections = document.querySelectorAll('.nonDisplayWhenHamburger')
+console.log(mobileMenu.innerHTMLileMenu);
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    btn.classList.remove('toggleMenu')
+    mobileMenu.classList.remove('openMenu')
+})
+
 btn.addEventListener('click', () =>{
-btn.classList.toggle('toggleMenu')
+    btn.classList.toggle('toggleMenu')
+    mobileMenu.classList.toggle('openMenu')
+    sections.forEach(section => {
+        section.style.display = 'none';
+    })
 });
