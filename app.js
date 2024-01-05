afb3e6eec6d6b8ce0ab53ab7c7e88695fdf582bc
Balovsky <mariuszfranik98@gmail.com>
2024-01-05 18:01:17 +0100

    added mobile view compatibile with galaxy fold, ip, and any android phone

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 console.log('connected to html');
 const btn = document.querySelector('.hamburgerMenu')
 const mobileMenu = document.querySelector('.mobileSubPages')
-const sections = document.querySelectorAll('.nonDisplayWhenHamburger')
+const mobileSubPages = document.querySelectorAll('.subPage')
 console.log(mobileMenu.innerHTMLileMenu);
 
 window.addEventListener('DOMContentLoaded', ()=>{
     btn.classList.remove('toggleMenu')
     mobileMenu.classList.remove('openMenu')
+    // mobileMenu.style.display = 'block';
 })
 
 btn.addEventListener('click', () =>{
     btn.classList.toggle('toggleMenu')
     mobileMenu.classList.toggle('openMenu')
-
+    
 });
+
+mobileSubPages.forEach(subPage => {
+    subPage.addEventListener('click', () =>{
+        mobileMenu.classList.remove('openMenu')
+        // mobileMenu.style.display = 'none';
+        btn.classList.remove('toggleMenu')
+    })
+})
